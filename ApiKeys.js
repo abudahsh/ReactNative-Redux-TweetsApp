83571bb6cc43c89c99e4ba6ea835a7d247fcb42e
abudahsh <willcodahsh@gmail.com>
2018-08-01 19:40:23 +0200

    adding firebase, new Gifs, new readme

---
+++ ApiKeys.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALLCDnoNELAFgG2OOpsCtk1NVfeKtW-Hs",
+  authDomain: "twitterclone-8606b.firebaseapp.com",
+  databaseURL: "https://twitterclone-8606b.firebaseio.com",
+  projectId: "twitterclone-8606b",
+  storageBucket: "twitterclone-8606b.appspot.com",
+  messagingSenderId: "1028920852895"
+};
+
+export default firebaseConfig;
--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { createSwitchNavigator, createStackNavigator } from "react-navigation";
+import * as firebase from "firebase";
 import store from "./redux/store";
 import LoginScreen from "./screens/LoginScreen";
 import TweetFeedScreen from "./screens/TweetFeedScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import TweetDetailScreen from "./screens/TweetDetailScreen";
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const TweetsNavigator = createStackNavigator({
   TweetFeed: TweetFeedScreen,
